57fe1572384364505032d6e73e45d6ea05beb8a1
Stephanie Englberger <stephaniekoebich@aol.com>
2025-08-26 22:47:12 +0200

    Add board model

--- app_board/models.py
+++ app_board/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User = get_user_model()
+
+class Board(models.Model):
+    title = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, related_name='owned_boards', on_delete=models.CASCADE)
+    members = models.ManyToManyField(User, related_name='boards')
+
+    def __str__(self):
+        return self.title
