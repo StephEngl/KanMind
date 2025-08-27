232fdc80351139c867263018884d6c087ec60fbf
Stephanie Englberger <stephaniekoebich@aol.com>
2025-08-27 14:28:50 +0200

    Implement task model

--- app_task/models.py
+++ app_task/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from app_board.models import Board
 
-# Create your models here.
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('to-do', 'To Do'),
+        ('in-progress', 'In Progress'),
+        ('review', 'Review'),
+        ('done', 'Done'),
+    ]
+
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
+    board = models.ForeignKey(Board, related_name='tasks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
+    assignee = models.ForeignKey(User, related_name='assigned_tasks', on_delete=models.SET_NULL, null=True, blank=True)
+    reviewer = models.ForeignKey(User, related_name='review_tasks', on_delete=models.SET_NULL, null=True, blank=True)
+    due_date = models.DateField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
