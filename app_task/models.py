62d81cffbeb0b0ae9e4ce27370add88929f21ce6
Stephanie Englberger <stephaniekoebich@aol.com>
2025-08-27 16:49:02 +0200

    Adapt task model

--- app_task/models.py
+++ app_task/models.py
@@ -3,21 +3,20 @@ from django.contrib.auth.models import User
 
 from app_board.models import Board
 
+STATUS_CHOICES = [
+    ('to-do', 'To Do'),
+    ('in-progress', 'In Progress'),
+    ('review', 'Review'),
+    ('done', 'Done'),
+]
+
+PRIORITY_CHOICES = [
+    ('low', 'Low'),
+    ('medium', 'Medium'),
+    ('high', 'High'),
+]
 
 class Task(models.Model):
-    STATUS_CHOICES = [
-        ('to-do', 'To Do'),
-        ('in-progress', 'In Progress'),
-        ('review', 'Review'),
-        ('done', 'Done'),
-    ]
-
-    PRIORITY_CHOICES = [
-        ('low', 'Low'),
-        ('medium', 'Medium'),
-        ('high', 'High'),
-    ]
-
     board = models.ForeignKey(Board, related_name='tasks', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
