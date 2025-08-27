a61003a9eef304466e483ab75ec556929deb7033
Stephanie Englberger <stephaniekoebich@aol.com>
2025-08-27 16:51:01 +0200

    Enhance board serializer

--- app_board/api/serializers.py
+++ app_board/api/serializers.py
@@ -1,12 +1,39 @@
+from django.contrib.auth.models import User
 from app_board.models import Board
 from rest_framework import serializers
 
 class BoardSerializer(serializers.ModelSerializer):
+    members = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=User.objects.all()
+    )
     member_count = serializers.SerializerMethodField()
-    
+    ticket_count = serializers.SerializerMethodField()
+    tasks_to_do_count = serializers.SerializerMethodField()
+    tasks_high_prio_count = serializers.SerializerMethodField()
+    owner_id = serializers.IntegerField(source='owner.id', read_only=True)
+
     class Meta:
         model = Board
-        fields = ["id", "title", "member_count"]
+        fields = [
+            'id',
+            'title',
+            'members',
+            'member_count',
+            'ticket_count',
+            'tasks_to_do_count',
+            'tasks_high_prio_count',
+            'owner_id',
+        ]
 
     def get_member_count(self, obj):
         return obj.members.count()
+
+    def get_ticket_count(self, obj):
+        return obj.tasks.count()
+
+    def get_tasks_to_do_count(self, obj):
+        return obj.tasks.filter(status='to-do').count()
+
+    def get_tasks_high_prio_count(self, obj):
+        return obj.tasks.filter(priority='high').count()
