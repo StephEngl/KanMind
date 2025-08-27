a73cd247fd3d8d752479c01b9c8acfec9648f4b8
Stephanie Englberger <stephaniekoebich@aol.com>
2025-08-27 13:15:37 +0200

    Remove dead code from auth urls

--- app_auth/api/urls.py
+++ app_auth/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import RegistrationView, LoginView
-from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
