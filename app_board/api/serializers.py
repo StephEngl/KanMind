d4631b2b2e7633fad0c4b1863b85b65ac5e6065a
Stephanie Englberger <stephaniekoebich@aol.com>
2025-08-27 22:26:24 +0200

    Implement board detail serializer

--- app_board/api/serializers.py
+++ app_board/api/serializers.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
+
 from app_board.models import Board
+from app_task.models import Task
+
 from rest_framework import serializers
+
 
 class BoardSerializer(serializers.ModelSerializer):
     members = serializers.PrimaryKeyRelatedField(
@@ -37,3 +41,34 @@ class BoardSerializer(serializers.ModelSerializer):
 
     def get_tasks_high_prio_count(self, obj):
         return obj.tasks.filter(priority='high').count()
+
+
+class UserSimpleSerializer(serializers.ModelSerializer):
+    fullname = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'fullname']
+
+    def get_fullname(self, obj):
+        return obj.username
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    assignee = UserSimpleSerializer(read_only=True)
+    reviewer = UserSimpleSerializer(read_only=True)
+    comments_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'status', 'priority',
+                'assignee', 'reviewer', 'due_date', 'comments_count']
+
+
+class BoardDetailSerializer(BoardSerializer):
+    owner_id = serializers.IntegerField(source='owner.id', read_only=True)
+    members = UserSimpleSerializer(many=True, read_only=True)
+    tasks = TaskSerializer(many=True, read_only=True)
+
+    class Meta(BoardSerializer.Meta):
+        fields = ['id', 'title', 'owner_id', 'members'] + ['tasks']
