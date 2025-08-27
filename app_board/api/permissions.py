6993de6c123ef30d9c55dae009a0fa9cd44de1b4
Stephanie Englberger <stephaniekoebich@aol.com>
2025-08-27 14:32:04 +0200

    Add IsOwnerOrMember of boards permission

---
+++ app_board/api/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+class IsMemberOrOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Allow full access if the user is the owner
+        if obj.owner == request.user:
+            return True
+        # Allow read-only access if the user is a member
+        return request.method in SAFE_METHODS and request.user in obj.members.all()
