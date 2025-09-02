8856e32f7fbb18ccfcf49f3c2ab848c1773da7e7
Stephanie Englberger <stephaniekoebich@aol.com>
2025-09-02 11:40:16 +0200

    Implememnt  nested router for task comments

--- app_task/api/urls.py
+++ app_task/api/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
-from .views import TaskListCreateView, TaskDetailView
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
+
+from .views import TaskViewSet, AssignedTaskListView, ReviewingTaskListView, CommentListView, CommentDeleteView, CommentViewSet
+
+router = DefaultRouter()
+router.register(r'', TaskViewSet, basename='task')
+
+comments_router = routers.NestedDefaultRouter(router, r'', lookup='task')
+comments_router.register(r'comments', CommentViewSet, basename='task-comments')
 
 urlpatterns = [
-    path('', TaskListCreateView.as_view(), name='task-list-create'),
-    path('<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
-]
+    path('', include(router.urls)),
+    path('', include(comments_router.urls)),
+    path('assigned-to-me', AssignedTaskListView.as_view(), name='assigned-tasks'),
+    path('reviewing', ReviewingTaskListView.as_view(), name='reviewing-tasks'),
+   ]
