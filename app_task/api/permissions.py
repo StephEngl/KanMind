72aa66a07e3dc0314ae01b55b78b6a078f23fa08
Stephanie Englberger <stephaniekoebich@aol.com>
2025-09-03 11:38:35 +0200

    Separate is-board-member-permission into one for comments and tasks

--- app_task/api/permissions.py
+++ app_task/api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 from app_board.models import Board
 
 
-class IsBoardMember(BasePermission):
+class IsBoardMemberForTask(BasePermission):
     """
     Allow access only to members of the board.
     """
@@ -11,7 +11,7 @@ class IsBoardMember(BasePermission):
     message = "You must be a member of this board."
 
     def has_permission(self, request, view):
-        if request.method == "POST":
+        if view.action == 'create':
             board_id = request.data.get("board")
             if not board_id:
                 self.message = "Board must be specified."
@@ -23,6 +23,21 @@ class IsBoardMember(BasePermission):
                 return False
             return request.user in board.members.all()
         return True
+    
+    def has_object_permission(self, request, view, obj):
+        return request.user in obj.board.members.all()
+
+
+class IsBoardMemberForComment(BasePermission):
+    message = "You must be a member of this board."
+    
+    def has_permission(self, request, view):
+        task = view.get_task()
+        if not task:
+            self.message = "Task must be specified"
+            return False
+        board = task.board
+        return request.user in board.members.all()
 
 
 class IsBoardOwnerOrTaskCreator(BasePermission):
@@ -41,7 +56,11 @@ class IsBoardOwnerOrTaskCreator(BasePermission):
 
 class IsCommentCreator(BasePermission):
     """
-    Custom permission to only allow the creator of a comment to access it.
+    Custom permission to only allow the creator of a comment to delete it.
     """
     message = "You must be the creator of this comment."
-    pass
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        is_comment_creator = (obj.author is not None and obj.author == user)
+        return is_comment_creator
